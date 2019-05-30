7fa31aaf08a8a1a879451c3cb72a411c3d2bd46c
Kayley <kayley.luftig@gmail.com>
2019-05-30 22:46:14 +0000

    first commit

---
+++ printing-nested-arrays.js
@@ -0,0 +1,12 @@
+function printItems(array) {
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      printItems(item);
+    } else {
+      console.log(item);
+    }
+  });
+}
+
+const array = ["😎", [["💩", ["🤗"]], [[["😼"]], "😂"]]];
+printItems(array);
